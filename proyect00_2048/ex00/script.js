e03762cb83cf7f33e45639d70ed62d3bd3055967
jmartos <kevlaroscuro@gmail.com>
2024-11-19 00:33:29 +0100

    works a little

--- proyect00_2048/ex00/script.js
+++ proyect00_2048/ex00/script.js
@@ -10,17 +10,17 @@ window.onload = function() {
 
 function init_game() {
     //board = [
-    //    [0, 0, 0, 0],
-    //    [0, 0, 0, 0],
-    //    [0, 0, 0, 0],
-    //    [0, 0, 0, 0]
+    //    [0,0,0,0],
+    //    [0,0,0,0],
+    //    [0,0,0,0],
+    //    [0,0,0,0]
     //]
 
     board = [
-        [2, 2, 2, 2],
-        [2, 2, 2, 2],
-        [4, 4, 4, 4],
-        [8, 8, 8, 8]
+        [2,2,2,2],
+        [2,2,2,2],
+        [4,4,4,4],
+        [8,8,8,8]
     ]
 
     for (let r = 0; r < rows; r++) {
@@ -81,12 +81,51 @@ function isEmpty() {
     return false;
 }
 
-document.addEventListener('up', (e) => {
-    if (e.code == "Left") {
+document.addEventListener('keyup', (e) => {     // e = event
+    if (e.code == "ArrowLeft") {
         slideLeft();
     }
 })
 
-function goLeft() {
-    for (let l + 0; )
+// Esta funcion recibira un array por parametros y devulelve una copia pero los que tengan 0.
+function eraseZeros(row) {
+    return row.filter(num => num != 0);
+}
+
+function slide(row) {
+    // [2,2,2,0]
+    row = eraseZeros(row);
+    
+    // [2,2,2,0] -> [2,2,2]
+    for (let i = 0; i < row.length - 1; i++) {
+        if (row[i] == row[i + 1]) {
+            row[i] *= 2;
+            row[i + 1] = 0;
+            score += row[i];
+        }
+    }
+
+    // [2,2,2] -> [4,0,2]
+    row = eraseZeros(row);
+
+    // [4,0,2] -> [4,2]
+    while (row.length < columns) {
+        row.push(0);
+    }
+
+    // [4,2] -> [4,2,0,0]
+    return (row);
+}
+
+function slideLeft() {
+    for (let r = 0; r < rows; r++) {
+        let row = board[r];
+        row = slide(row);
+        board[r] = row;
+        for (let c = 0; c < columns; c++) {
+            let box = document.getElementById(r.toString() + "-" + c.toString());
+            let num = board[r][c];
+            updateBox(box, num);
+        }
+    }
 }
